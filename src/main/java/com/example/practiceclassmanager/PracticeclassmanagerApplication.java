ba4e192820ec6260604aad7f5648b3c0fa3b3162
suganthi1410 <suganthi1410@gmail.com>
2026-08-26 13:32:31 -0500

    initialcommit

---
+++ src/main/java/com/example/practiceclassmanager/PracticeclassmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.practiceclassmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeclassmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeclassmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practiceclassmanager/PracticeclassmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practiceclassmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeclassmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
